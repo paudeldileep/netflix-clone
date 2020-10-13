12d88618b105abbd31143727e1c23a1c214a1632
paudeldileep <dileepkpaudel@gmail.com>
2020-10-13 22:40:20 +0545

    final v.1

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
+const[show,setShow]=useState(false);
+
+useEffect(()=>{
+    window.addEventListener("scroll",()=>{
+        if(window.scrollY>100){
+            setShow(true);
+        }
+        else{
+            setShow(false);
+        }
+    });
+
+    return()=>{
+        window.removeEventListener("scroll");
+    }
+},[]);
+
     return (
-        <div>
-            Navbar
+        <div className={`navbar ${show && 'navbar-black'}`}>
+            <img src='/n-clone.png' className='navbar-logo' alt='netflix-clone logo'/>
+            <img src='/avatar.png' className='navbar-avatar' alt='avatar-logo'/>
         </div>
     )
 }
--- src/Row.js
+++ src/Row.js
@@ -1,36 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import axios from './axios'
-import './Row.css'
+import React, { useEffect, useState } from "react"
+import axios from "./axios"
+import "./Row.css"
+import Youtube from "react-youtube"
+import movieTrailer from "movie-trailer"
 
-const img_base_url='https://image.tmdb.org/t/p/original/';
+const img_base_url = "https://image.tmdb.org/t/p/original/"
 
-const Row = ({title,url,islarge}) => {
-    
-    
-    const[movies,setMovies]=useState([])
+const Row = ({ title, url, islarge }) => {
+  const [movies, setMovies] = useState([])
+  const [tUrl, setTurl] = useState("")
 
-    useEffect(()=>{
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await axios.get(url)
+      setMovies(res.data.results)
+    }
 
-        const fetchMovies=async()=>{
-            const res=await axios.get(url);
-           setMovies(res.data.results);
-        }
+    fetchMovies()
+  }, [url])
 
-        fetchMovies()
-    },[url])
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  }
 
-    
+  const handleClick = (movie) => {
+    if (tUrl) {
+      setTurl("")
+    } else {
+        console.log(movie?.name)
+      movieTrailer(movie?.name || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTurl(urlParams.get("v")) 
+        })
+        .catch((error) => console.log(error))
+    }
+  }
 
-    return (
-        <div className='main-div'>
-            <h2>{title}</h2>
-            <div className='data-div'>
-                {movies.map(movie=>(
-                    <img className={`movie-img ${islarge && 'movie-img-large'}`} key={movie.id} src={`${img_base_url}${islarge ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className='main-div'>
+      <h2>{title}</h2>
+      <div className='data-div'>
+        {movies.map((movie) => (
+          <img
+            className={`movie-img ${islarge && "movie-img-large"}`}
+            key={movie.id}
+            src={`${img_base_url}${
+              islarge ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            onClick={() => handleClick(movie)}
+          />
+        ))}
+      </div>
+      {tUrl && <Youtube videoId={tUrl} opts={opts} />}
+    </div>
+  )
 }
 
 export default Row
